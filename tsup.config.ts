9829997f914e065e661707c41f1a66e58dd44da4
kevindmorris <kevin.d.morris4@gmail.com>
2025-01-26 12:07:37 -0500

    Update build config

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
+  target: "es2020",
+  format: ["cjs", "esm"],
   splitting: false,
   sourcemap: true,
   clean: true,
+  dts: true,
 });
